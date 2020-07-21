f0d2f226a7aef7ecc39c324b7d80b7f0b610e6d5
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2020-07-21 17:55:18 -0400

    Add pagination in GET /papers

--- risotto/controllers/papers_controller.py
+++ risotto/controllers/papers_controller.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify
+import math
+
+from flask import Blueprint, jsonify, request
 
 from risotto.models import Paper
 
@@ -7,6 +9,20 @@ bp = Blueprint("papers", __name__, url_prefix="/papers")
 
 @bp.route("/", methods=("GET",))
 def get_papers():
-    papers = Paper.all()
-    response = {"status": "OK", "payload": papers.iloc[:10].to_dict(orient="records")}
+    # Query params
+    page = int(request.args.get("page", "1"))
+    items_per_page = int(request.args.get("items_per_page", "20"))
+    papers_df = Paper.all()
+    masked_df = papers_df
+    num_papers = len(masked_df)
+    num_pages = math.ceil(num_papers / items_per_page)
+    start_idx, end_idx = (page - 1) * items_per_page, page * items_per_page
+    papers_page = masked_df.iloc[start_idx:end_idx]
+    papers_serialized = papers_page.to_dict(orient="records")
+    response = {
+        "status": "OK",
+        "payload": papers_serialized,
+        "num_pages": num_pages,
+        "page": page,
+    }
     return jsonify(response)
--- tests/test_papers.py
+++ tests/test_papers.py
@@ -4,4 +4,6 @@ def test_get_papers(client):
 
     assert contents["status"] == "OK"
     assert type(contents["payload"]) is list
+    assert type(contents["num_pages"]) is int
+    assert type(contents["page"]) is int
     assert response.status_code == 200
