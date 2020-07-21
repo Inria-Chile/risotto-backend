9de9265e6cca40cac85149afc2ed959c9b5c2704
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2020-07-21 18:28:43 -0400

    Add filtering by topic, subtopic and text

--- risotto/controllers/papers_controller.py
+++ risotto/controllers/papers_controller.py
@@ -10,19 +10,41 @@ bp = Blueprint("papers", __name__, url_prefix="/papers")
 @bp.route("/", methods=("GET",))
 def get_papers():
     # Query params
-    page = int(request.args.get("page", "1"))
-    items_per_page = int(request.args.get("items_per_page", "20"))
+    page_param = int(request.args.get("page", "1"))
+    items_per_page_param = int(request.args.get("items_per_page", "20"))
+    topic_param = request.args.get("topic")
+    subtopic_param = request.args.get("subtopic")
+    search_param = request.args.get("search")
+
+    if topic_param is None and subtopic_param is not None:
+        response = {
+            "status": "error",
+            "msg": "Can't specify subtopic parameter if a topic parameter is not provided",
+        }
+        return jsonify(response), 400
+
+    # Filtering
     papers_df = Paper.all()
-    masked_df = papers_df
-    num_papers = len(masked_df)
-    num_pages = math.ceil(num_papers / items_per_page)
-    start_idx, end_idx = (page - 1) * items_per_page, page * items_per_page
-    papers_page = masked_df.iloc[start_idx:end_idx]
+    filtered_df = Paper.filter(
+        papers_df, topic=topic_param, subtopic=subtopic_param, search=search_param
+    )
+
+    # Perform pagination
+    num_papers = len(filtered_df)
+    num_pages = math.ceil(num_papers / items_per_page_param)
+    start_idx, end_idx = (
+        (page_param - 1) * items_per_page_param,
+        page_param * items_per_page_param,
+    )
+    papers_page = filtered_df.iloc[start_idx:end_idx]
     papers_serialized = papers_page.to_dict(orient="records")
+
     response = {
         "status": "OK",
         "payload": papers_serialized,
+        "num_results": num_papers,
         "num_pages": num_pages,
-        "page": page,
+        "page": page_param,
     }
+
     return jsonify(response)
--- risotto/models.py
+++ risotto/models.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from typing import Optional, List
 from flask import current_app
 
 from risotto import db
@@ -10,8 +11,29 @@ class Paper(db.Model):
     title = db.Column(db.Text)
     abstract = db.Column(db.Text)
 
+    # TODO: join DFs
     @staticmethod
     def all() -> pd.DataFrame:
         # all_papers = current_app._artifacts.papers_artifact.to_json(orient="records")
         all_papers = current_app._artifacts.papers_artifact
         return all_papers
+
+    @staticmethod
+    def filter(
+        papers: pd.DataFrame,
+        topic: Optional[str],
+        subtopic: Optional[str],
+        search: Optional[str],
+    ) -> pd.DataFrame:
+        if topic is not None:
+            topic_mask = papers["topic"] == int(topic)
+            papers = papers[topic_mask]
+        if subtopic is not None:
+            subtopic_mask = papers["subtopic"] == int(subtopic)
+            papers = papers[subtopic_mask]
+        if search is not None and len(search) > 0:
+            search_mask = (
+                (papers["title"] + papers["abstract"]).str.lower().str.contains(search)
+            )
+            papers = papers[search_mask]
+        return papers
