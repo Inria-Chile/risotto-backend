2ce0aa3ac4ddc3bb544b3addac18d22b92805e88
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2020-07-22 09:41:00 -0400

    Remove unused db and migration initialization

--- risotto/__init__.py
+++ risotto/__init__.py
@@ -5,16 +5,16 @@ from flask_migrate import Migrate
 import risotto.settings
 from risotto.artifacts import load_artifacts
 
-db = SQLAlchemy()
-migrate = Migrate()
+# db = SQLAlchemy()
+# migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_mapping(risotto.settings.freeze())
 
-    db.init_app(app)
-    migrate.init_app(app, db)
+    # db.init_app(app)
+    # migrate.init_app(app, db)
     load_artifacts(app)
 
     @app.route("/health")
--- risotto/models.py
+++ risotto/models.py
@@ -2,19 +2,10 @@ import pandas as pd
 from typing import Optional, List
 from flask import current_app
 
-from risotto import db
 
-
-class Paper(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    cord_id = db.Column(db.String(length=256))
-    title = db.Column(db.Text)
-    abstract = db.Column(db.Text)
-
-    # TODO: join DFs
+class Paper:
     @staticmethod
     def all() -> pd.DataFrame:
-        # all_papers = current_app._artifacts.papers_artifact.to_json(orient="records")
         all_papers = current_app._artifacts.papers_artifact
         return all_papers
 
